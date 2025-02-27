acc4bc04ae6c5bdf4c2e9e408339bd6541b18ab1
Simon <mail@simondirks.com>
2025-02-27 20:47:29 +0100

    Prettier run

--- index.js
+++ index.js
@@ -1,60 +1,65 @@
-const ignoreShortcutsForClassNames = ['eventpopover-header--draggable', 'modal-two'];
-const ignoreElementsQuery = ignoreShortcutsForClassNames.map(className => `.${className}`).join(',')
+const ignoreShortcutsForClassNames = [
+  "eventpopover-header--draggable",
+  "modal-two",
+];
+const ignoreElementsQuery = ignoreShortcutsForClassNames
+  .map((className) => `.${className}`)
+  .join(",");
 
 document.addEventListener("keydown", function (event) {
-    const ignoreElements = document.querySelectorAll(ignoreElementsQuery);
-    const ignoreElementsAreShown = ignoreElements.length > 0
-    if (ignoreElementsAreShown) {
-        return;
-    }
+  const ignoreElements = document.querySelectorAll(ignoreElementsQuery);
+  const ignoreElementsAreShown = ignoreElements.length > 0;
+  if (ignoreElementsAreShown) {
+    return;
+  }
 
-    let buttonQueries = [];
-    const key = event.key.toLowerCase();
+  let buttonQueries = [];
+  const key = event.key.toLowerCase();
 
-    // Next
-    if (key === "n") {
-        buttonQueries.push('[data-testid="calendar-toolbar:next"]');
-    }
+  // Next
+  if (key === "n") {
+    buttonQueries.push('[data-testid="calendar-toolbar:next"]');
+  }
 
-    // Previous
-    if (key === "p") {
-        buttonQueries.push('[data-testid="calendar-toolbar:previous"]');
-    }
+  // Previous
+  if (key === "p") {
+    buttonQueries.push('[data-testid="calendar-toolbar:previous"]');
+  }
 
-    // Today
-    if (key === "t") {
-        buttonQueries.push('[data-testid="calendar-toolbar:today"]');
-    }
+  // Today
+  if (key === "t") {
+    buttonQueries.push('[data-testid="calendar-toolbar:today"]');
+  }
 
-    // Day
-    if (key === 'd') {
-        buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
-        buttonQueries.push('[data-testid="view-option-day"]');
-        buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
-    }
+  // Day
+  if (key === "d") {
+    buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
+    buttonQueries.push('[data-testid="view-option-day"]');
+    buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
+  }
 
-    // Week
-    if (key === 'w') {
-        buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
-        buttonQueries.push('[data-testid="view-option-week"]');
-        buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
-    }
+  // Week
+  if (key === "w") {
+    buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
+    buttonQueries.push('[data-testid="view-option-week"]');
+    buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
+  }
 
-    // Month
-    if (key === 'm') {
-        buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
-        buttonQueries.push('[data-testid="view-option-month"]');
-        buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
-    }
+  // Month
+  if (key === "m") {
+    buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
+    buttonQueries.push('[data-testid="view-option-month"]');
+    buttonQueries.push('[data-testid="calendar-view:view-options-dropdown"]');
+  }
 
-    if (!buttonQueries || buttonQueries.length === 0) {
-        return;
-    }
+  if (!buttonQueries || buttonQueries.length === 0) {
+    return;
+  }
 
-    for (const buttonQuery of buttonQueries) {
-        const button = document.querySelector(buttonQuery);
-        if (button) {
-            button.click();
-        }
+  for (const buttonQuery of buttonQueries) {
+    const button = document.querySelector(buttonQuery);
+    if (button) {
+      button.click();
     }
+  }
 });
